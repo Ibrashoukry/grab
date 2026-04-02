ed8566d5ada3cc8fcf24b3a730b90c74afd3dec8
Ibrashoukry <ibrahim.m.shoukry@gmail.com>
2026-04-01 18:14:02 -0700

    Use BufReader instead of loading entire file

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,32 @@
+use clap::Parser;
+use std::io::{self, BufRead, BufReader};
+use std::fs::File;
 
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
 struct Cli {
+	/// The pattern to look for
 	pattern: String,
+	/// The path to the file to read
 	path: std::path::PathBuf,
 }
 
-fn main() {
+fn main() -> io::Result<()> {
 
-	let pattern = std::env::args().nth(1).expect("no pattern given");
-	let path = std::env::args().nth(2).expect("no path given");
+	let args = Cli::parse();
 
-	let args = Cli {
-		pattern: pattern,
-		path: std::path::PathBuf::from(path),
-	};
+	let file = File::open(&args.path)?;
+	let reader = BufReader::new(file);
 
-	println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
+	// let content = std::fs::read_to_string(&args.path).expect("could not read file");
+
+	for line in reader.lines() {
+		let line = line?;
+		if line.contains(&args.pattern) {
+			println!("{}",line);
+		}
+	}
+
+	Ok(())
+
 }
